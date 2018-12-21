f6b78c66f827e7523ef62bdfb7f65b700b60f0ca
Murdock73 <Murdock73@users.noreply.github.com>
2018-12-21 12:12:26 +0100

    Add files via upload

---
+++ pagel.h
@@ -0,0 +1,28 @@
+#ifndef header_h
+#define header_h
+
+String file1=
+"<html>\r\n"
+"<head>\r\n"
+"	<title></title>\r\n"
+"</head>\r\n"
+"<body>\r\n"
+"<h2><code>Da Flinos project - Camera da letto</code></h2>\r\n"
+"\r\n"
+"<p><code><span style=\"font-size:14px;\">Temperatura attuale</span>&nbsp;nn,nn C&deg;&nbsp;</code></p>\r\n"
+"\r\n"
+"<p><code>Parametri climatizzatore</code></p>\r\n"
+"\r\n"
+"<p><code>Mode XXXX&nbsp;<input name=\"AcMode\" type=\"button\" value=\"Cambia\" /></code></p>\r\n"
+"\r\n"
+"<p><code>Temp NN&nbsp;<input name=\"TempPlus\" type=\"button\" value=\"+\" />&nbsp;<input name=\"TempMinus\" type=\"button\" value=\"-\" /></code></p>\r\n"
+"\r\n"
+"<p><code>Acc&nbsp; &nbsp;<input name=\"AcOn\" type=\"button\" value=\"ON\" /></code></p>\r\n"
+"\r\n"
+"<p><code>Spent&nbsp;<input name=\"AcOff\" type=\"button\" value=\"OFF\" /></code></p>\r\n"
+"\r\n"
+"<p><span style=\"font-size:8px;\">Versione 0.1.1</span></p>\r\n"
+"</body>\r\n"
+"</html>\r\n";
+
+#endif
