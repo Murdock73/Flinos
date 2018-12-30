1ab7deca3761909d5aa535ab167de152a890b541
Murdock73 <Murdock73@users.noreply.github.com>
2018-12-30 11:49:05 +0100

    Update Filnos1.ino

--- Filnos1.ino
+++ Filnos1.ino
@@ -15,12 +15,19 @@
 #include <DallasTemperature.h>
 
 // Replace with your network details
-const char* ssid = "********";
-const char* password = "*********";
+const char* ssid = "*************";
+const char* password = "***********";
 
+// Variable to store the HTTP request
+String header;
 
-#define ONE_WIRE_BUS 5      // Data wire GPIO pin to use. GPIO5 (D1)
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. GPIO4 (D2).
+// Auxiliar variables to store the current output state
+String HvacState = "off";
+uint8_t HvacMode = 1;
+uint8_t HvacTemp = 25;
+
+#define ONE_WIRE_BUS D1      // Data wire GPIO pin to use. GPIO5 (D1)
+const uint16_t kIrLed = D2;  // ESP8266 GPIO pin to use. GPIO4 (D2).
 IRSamsungAc ac(kIrLed);    // Set the GPIO to be used to sending the message
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
@@ -36,6 +43,7 @@ WiFiServer server(80);
 // only runs once on boot
 void setup() {
   // Initializing serial port for debugging purposes
+  ac.begin();
   Serial.begin(115200);
   delay(10);
 
@@ -81,58 +89,36 @@ void loop() {
   WiFiClient client = server.available();
   
   if (client) {
-    Serial.println("New client");
-    // bolean to locate when the http request ends
-    boolean blank_line = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.println(c);
-        if (c == '\n' && blank_line) {
-            getTemperature();
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: text/html");
+            client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            // your actual web page that displays temperature
-            client.println("<!DOCTYPE HTML>");
-            client.println("<HTML>\r\n");
-            client.println("<head>\r\n");
-            client.println("<title>Camera AC</title>\r\n");
-            client.println("</head>\r\n");
-            client.println("<body>\r\n");
-            client.println("<h2><code>Da Flinos project - Camera da letto</code></h2>\r\n");
-            client.println("\r\n");
-            client.println("<p><code><span style=\"font-size:14px;\">Temperatura attuale</span>&nbsp;
-            client.println(temperatureCString);
-            client.println(" C&deg;&nbsp;</code></p>\r\n"
-            client.println("\r\n");
-            client.println("<p><code>Parametri climatizzatore</code></p>\r\n");
-            client.println("\r\n");
-            client.println("<p><code>Mode &nbsp;
-            client.println(HvacMode);                  
-            client.println("<input name=\"AcMode\" type=\"button\" value=\"Cambia\" /></code></p>\r\n");
-            client.println("\r\n");
-            client.println("<p><code>Temp ");
-            client.println(HvacTemp);                                    
-            client.println(" &nbsp;<input name=\"TempPlus\" type=\"button\" value=\"+\" />&nbsp;<input name=\"TempMinus\" type=\"button\" value=\"-\" /></code></p>\r\n");
-            client.println("\r\n");                  
-            client.println("<p><code>Acc&nbsp; &nbsp;<input name=\"AcOn\" type=\"button\" value=\"ON\" /></code></p>\r\n");
-            client.println("\r\n");
-            client.println("<p><code>Spent&nbsp;<input name=\"AcOff\" type=\"button\" value=\"OFF\" /></code></p>\r\n");
-            client.println("\r\n");
-            client.println("<p><span style=\"font-size:8px;\">Versione 0.1.1</span></p>\r\n");
-            client.println("</body>\r\n");
-            client.println("</html>\r\n");
-            break;
-            if (readString.indexOf("?ON") >0){
+            
+            // turns the AC on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("AC is on");
+              HvacState = "on";
+
                 Serial.println("Sending...");
 
                 // Set up what we want to send. See ir_Samsung.cpp for all the options.
                 ac.on();
                 ac.setFan(kSamsungAcFanAuto);
                 ac.setMode(kSamsungAcCool);
-                ac.setTemp(25);
+                ac.setTemp(HvacTemp);
                 ac.setSwing(false);
                 
                 // Display what we are going to send.
@@ -140,23 +126,155 @@ void loop() {
                 // Now send the IR signal.
                 #if SEND_SAMSUNG
                   ac.send();
+                  Serial.println("INVIO");
                 #endif  // SEND_SAMSUNG
-                delay(5000);
-           }
-        }
-        if (c == '\n') {
-          // when starts reading a new line
-          blank_line = true;
-        }
-        else if (c != '\r') {
-          // when finds a character on the current line
-          blank_line = false;
+                //delay(1000);
+              
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("AC is off");
+              HvacState = "off";
+              Serial.println("Sending...");
+
+                // Set up what we want to send. See ir_Samsung.cpp for all the options.
+                ac.off();
+                ac.setFan(kSamsungAcFanAuto);
+                ac.setMode(kSamsungAcCool);
+                ac.setTemp(HvacTemp);
+                ac.setSwing(false);
+                
+                // Display what we are going to send.
+                Serial.println(ac.toString());
+                // Now send the IR signal.
+                #if SEND_SAMSUNG
+                  ac.send();
+                  Serial.println("INVIO");
+                #endif  // SEND_SAMSUNG
+                //fdelay(1000);
+              
+              
+            } else if (header.indexOf("GET /4/caldo") >= 0) {
+              if (HvacState == "off") {
+                Serial.println("Ac Mode CALDO");
+                HvacMode = 4;
+              }
+            } else if (header.indexOf("GET /4/freddo") >= 0) {
+              if (HvacState == "off") {
+                Serial.println("Ac Mode FREDDO");
+                HvacMode = 1;
+              }
+            } else if (header.indexOf("GET /3/minus") >= 0) {
+              Serial.println("Temp -");
+              if (HvacTemp > 16) {
+                  HvacTemp = HvacTemp - 1;
+              }
+              if (HvacState == "on") {
+              Serial.println("Sending...");
+              ac.on();
+              ac.setFan(kSamsungAcFanAuto);
+              ac.setMode(kSamsungAcCool);
+              ac.setTemp(HvacTemp);
+              ac.setSwing(false);
+                
+              // Display what we are going to send.
+              Serial.println(ac.toString());
+              // Now send the IR signal.
+              #if SEND_SAMSUNG
+                 ac.send();
+                 Serial.println("INVIO");
+              #endif  // SEND_SAMSUNG
+              //delay(500);
+              }
+            } else if (header.indexOf("GET /3/plus") >= 0) {
+              Serial.println("Temp +");
+              if (HvacTemp < 30) {
+                  HvacTemp = HvacTemp + 1;
+              }
+                            if (HvacState == "on") {
+              Serial.println("Sending...");
+              ac.on();
+              ac.setFan(kSamsungAcFanAuto);
+              ac.setMode(kSamsungAcCool);
+              ac.setTemp(HvacTemp);
+              ac.setSwing(false);
+                
+              // Display what we are going to send.
+              Serial.println(ac.toString());
+              // Now send the IR signal.
+              #if SEND_SAMSUNG
+                 ac.send();
+                 Serial.println("INVIO");
+              #endif  // SEND_SAMSUNG
+              //delay(500);
+              }
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}");
+            client.println(".button3 {background-color: #0000FF;}");
+            client.println(".button4 {background-color: #CC0000;}");
+            client.println(".button5 {background-color: #0066CC;}</style></head>");
+
+                        
+            // Web Page Heading
+            client.println("<body><h1>Da Flinos - Camera</h1>");
+
+            getTemperature();
+            client.println("<p>Temperatura attuale </p>");
+            client.println("<p><a href=\"/6/on\"><button class=\"button button5\">");
+            client.println(temperatureCString);
+            client.println("</button></a></p>");
+
+            client.println("<p>Modalita' AC </p>");
+                                                            
+            // Display current state, and ON/OFF button  
+            if (HvacState=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button button2\">OFF</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button\">ON</button></a></p>");
+            } 
+               
+            // Display current mode, and COOL/HEAT button  
+            // If the AcMode is Cool, it displays the Cool button      
+            Serial.println(HvacMode); 
+            if (HvacMode==1) {
+              client.println("<p><a href=\"/4/caldo\"><button class=\"button button3\">FREDDO</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/freddo\"><button class=\"button button4\">CALDO</button></a></p>");
+            }
+
+            client.println("<p><a href=\"/3/minus\"><button class=\"button button5\">-</button></a></p>");
+            client.println("<p><a href=\"/3/on\"><button class=\"button button5\">");
+            client.println(HvacTemp);
+            client.println("</button></a></p>");
+            client.println("<p><a href=\"/3/plus\"><button class=\"button button5\">+</button></a></p>");
+            
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
         }
       }
-    }  
-    // closing the client connection
-    delay(1);
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }   
